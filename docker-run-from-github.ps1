6cffbbe14b2d8d357bc42d0fd8a1e6979e772296
Nok Ádám <Adam.Nok@4ig.hu>
2025-05-17 18:52:06 +0200

    Change repository name - it must be lowercase

--- docker-run-from-github.ps1
+++ docker-run-from-github.ps1
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 --rm --name demo -it ghcr.io/4iG-AdamNok/cubix/cloudnative/demo:actions
+docker run -p 8080:8080 --rm --name demo -it ghcr.io/4ig-adamnok/cubix/cloudnative/demo:actions
